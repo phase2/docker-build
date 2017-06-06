a1ac4462c323c41007a467bd186263f9866f5435
Frank Febbraro <frankfebbraro@gmail.com>
2017-06-05 20:59:02 -0700

    enhanced to either build all dockerfiles, or just the tag/dir passed in as an arg

--- build.sh
+++ build.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 
-docker build -t outrigger/build:php70 .
+set -e
+
+# Get the versions to build
+
+# If they are passed as args
+if [ $# -gt 0 ]; then
+  buildDirs="$@"
+else
+  # else, get all the relevant dirs
+  buildDirs=$(ls | grep -E '^php[0-9]+$')
+fi
+
+# Build each one of them
+for version in $buildDirs; do
+  pushd $version
+  docker build -t outrigger/build:${version} .
+  popd
+done
+
